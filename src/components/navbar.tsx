9d0e48c342c124161d2a0c6250a2401469f202be
kluis6 <luislmorningstar@gmail.com.br>
2024-08-31 07:04:58 -0300

    q

---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div className="w-full bg-neutral-800 border-b border-white">
+      <div className="px-4 py-2.5">
+        <h1 className="text-white font-bold text-base">BetaSorte</h1>
+      </div>
+    </div>
+  );
+}
